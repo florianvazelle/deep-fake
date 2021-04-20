c74c44fa4d70037fbf17bdde505cbd3e694995ea
Shen <s_lin97@hotmail.com>
2021-04-20 17:18:04 +0200

    Tracking Head(Img) not perfect yet

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,13 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <Image.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/objdetect.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
 
 cv::Mat openImg(std::string path) {
   cv::Mat img = cv::imread(path, 0);
@@ -13,11 +20,26 @@ cv::Mat openImg(std::string path) {
 
 int main(int argc, char** argv) {
   // create an image
-  Image img("assets/therock.jpg");
+  //Image img("assets/therock.jpg");
 
+  string path ="assets/therock.jpg";
+  Mat img = imread(path);
+  CascadeClassifier faceCascade;
+  faceCascade.load("assets/models/haarcascade_frontalface_default.xml");
+
+  if(faceCascade.empty()){cout<<"xml file not loaded"<<endl;}
+
+  vector<Rect> faces;
+  faceCascade.detectMultiScale(img,faces,1.1,10);
+
+  for(int i =0; i< faces.size(); i++){
+    rectangle(img,faces[i].tl(), faces[i].br(),Scalar(255,0,255),2);
+  }
+
+  
   // display the image
-  img.show();
-
+  //img.show();
+  imshow("Image", img);
   // wait for the user to press any key:
   cv::waitKey(0);
 
