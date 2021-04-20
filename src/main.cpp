71951b934da13eb1a3fd72c8cb4acde15ffadd14
florianvazelle <ponythugflorian@gmail.com>
2021-04-20 14:40:11 +0200

    add dlib and command line option

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+#include <dlib/cmd_line_parser.h>
 #include <opencv2/core/types_c.h>
 
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
+using namespace dlib;
 
 Mat openImg(std::string path) {
   Mat img = imread(path, 0);
@@ -13,11 +15,36 @@ Mat openImg(std::string path) {
 }
 
 int main(int argc, char** argv) {
+  /* Define the command line options */
+  command_line_parser parser;
+
+  parser.add_option("h", "Display this help message.");
+  parser.add_option("in", "The input image", 1);
+
+  parser.parse(argc, argv);
+
+  if (parser.option("h")) {
+    std::cout << "Usage: deep_fake --in input_file\n";
+    parser.print_options();
+    return 0;
+  }
+
+  /* Get the input image */
+  std::string in_file;
+
+  if (parser.option("in")) {
+    in_file = parser.option("in").argument();
+  } else {
+    std::cout << "Error in command line:\n   You must specify an input file.\n";
+    std::cout << "\nTry the -h option for more information." << std::endl;
+    return 0;
+  }
+
   // create a gui window:
   namedWindow("Output", 1);
 
   //std::string image_path = samples::findFile("assets/therock.jpg");
-  Mat img = openImg("assets/therock.jpg");
+  Mat img = openImg(in_file);
 
   // initialize a 120X350 matrix of black pixels:
   Mat output = Mat::zeros(120, 350, CV_8UC3);
